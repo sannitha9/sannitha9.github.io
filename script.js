3ae66a18028f91c4d995e7074c4c8fc4f995ed0b
Sannitha9 <ysreddy8523@gmail.com>
2025-07-14 23:14:51 +0530

    Eliminated minor errors

--- script.js
+++ script.js
@@ -7,22 +7,30 @@ document.addEventListener('DOMContentLoaded', load);
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   const taskText = taskInput.value.trim();
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  let index=tasks.length+1;
   if (taskText !== '') {
-    addTask(taskText, false);
-    save(taskText,false);
+    addTask(index, taskText, false);
+    save(index, taskText, false);
     taskInput.value = '';
   }
 });
 
-function addTask(text, completed) {
+function addTask(index, text, completed) {
   const li = document.createElement('li');
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  checkbox.checked = completed;
+  checkbox.checked=completed;
+  if(completed){
+    li.classList.add('completed');
+  }
+  else{
+    li.classList.remove('completed');
+  }
   checkbox.addEventListener('change', () => {
     li.classList.toggle('completed');
-    update(text, checkbox.checked);
+    update(index, text, checkbox.checked);
   });
 
   const span = document.createElement('div');
@@ -33,7 +41,7 @@ function addTask(text, completed) {
   deleteBtn.classList.add('delete-btn');
   deleteBtn.addEventListener('click', () => {
     taskList.removeChild(li);
-    remove(text);
+    remove(index);
   });
 
   li.appendChild(checkbox);
@@ -43,41 +51,44 @@ function addTask(text, completed) {
   taskList.appendChild(li);
 }
 
-function save(text, completed){
+function save(index, text, completed){
   const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.push({text, completed});
+  tasks.push({index, text, completed});
   localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
-function update(text, completed){
+function update(index, text, completed){
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  let task;
+  let ind;
   for(let i=0;i<tasks.length;i++){
-    if(tasks[i].text==text){
-      task=tasks[i];
+    if(tasks[i].text==text && tasks[i].index==index){
+      ind=i;
       break;
     }
   }
-  task.completed=completed;
+  tasks[ind].completed=completed;
   localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
-function remove(text){
+function remove(index){
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
   let t=[];
   for(let i=0;i<tasks.length;i++){
-    if(tasks[i].text != text){
+    if(tasks[i].index!=index){
       t.push(tasks[i]);
     }
   }
   tasks = t;
+  for(let i=0;i<tasks.length;i++){
+    tasks[i].index=i+1;
+  }
   localStorage.setItem('tasks',JSON.stringify(t));
 }
 
 function load(){
   const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
   for(let i=0;i<tasks.length;i++){
-    addTask(tasks[i].text, tasks[i].completed);
+    addTask(tasks[i].index, tasks[i].text, tasks[i].completed);
   }
 }
 
